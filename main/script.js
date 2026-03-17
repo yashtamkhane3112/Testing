80ff38c0faa26e02b9dd7a401480f097726dfa2c
yashtamkhane3112 <yashtamkhane18@gmail.com>
2026-03-17 17:31:04 +0530

    update project

---
+++ main/script.js
@@ -0,0 +1,8 @@
+console.log("SiteGuard Ready");
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+// button click demo
+document.querySelectorAll("button").forEach(btn=>{
+btn.addEventListener("click",()=>{
+alert("Button clicked!");
+});
+});
