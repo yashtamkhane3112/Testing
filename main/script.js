7bc7407d9321b815f6a446c3f27bec6701ea4659
yashtamkhane3112 <yashtamkhane18@gmail.com>
2026-03-20 13:23:50 +0530

    refactor code

--- main/script.js
+++ main/script.js
@@ -1,8 +1,6 @@
-console.log("SiteGuard Ready");
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-// button click demo
+// simple click test
 document.querySelectorAll("button").forEach(btn=>{
 btn.addEventListener("click",()=>{
-alert("Button clicked!");
+console.log(btn.innerText + " clicked");
 });
 });
